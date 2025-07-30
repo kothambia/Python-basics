d1f21c6fb14ee5ffe2315fe0877fb19e74840883
kothambia <kothambiaaryan@gmail.com>
2025-07-30 23:53:15 +0530

    Add files via upload

---
+++ f_strings.py
@@ -0,0 +1,16 @@
+#fstring
+
+
+txt = "my name is {1} and my age is {0}"
+name = "sachin"
+age = 20
+# print(txt.format(name,age))
+print(txt.format(age,name))
+
+
+# rupee = 69.69
+# txt = f"i lost my {rupee:.f} rupees"
+# print(txt)
+# latter = "position"
+# beach = 69
+# print(f"the {beach} {latter} part")
