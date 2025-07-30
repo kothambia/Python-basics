b4384907344773d54f905e9f0bc95d5b579dde07
kothambia <kothambiaaryan@gmail.com>
2025-07-30 23:51:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+#string concept , multiple text , pronting just characters of text, for loop, length function 
+
+name = "Aryan"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+mango = '''mango is very delicious
+ mango is my favourite food 
+ mango is yellow'''
+print(len(mango))
+
+
+for characters in mango:
+    print(characters)
+
+Name = "Tushar,Jigar,Brijesh"
+A = len(Name)-5
+print(A)
+print(len(Name))
+print(Name[-12:-9])
+print(Name[0:-5])
+print(Name[0:15])
+print(Name[0:len(Name)-5])
+print(Name[-7:20])
+
+bitch = len(Name)
+print("my belowed sibilings, i remember having", bitch, "chocolates")
+
+Chiken = "Chiken is very good and healthy food"
+print(len(Chiken))
+print(Chiken[10:19])
+print(Chiken[-26:-17])
+print(Chiken[len(Chiken)-26:len(Chiken)-17])
