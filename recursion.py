2ee3afce4667c6ef14b35a731a753bc69e9611c0
kothambia <kothambiaaryan@gmail.com>
2025-07-31 20:11:39 +0530

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,37 @@
+# def factorial(n):
+#     if n==0 or n==1:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+        
+# v = int(input("Enter bhosmarina enter value:"))
+# print(factorial(v))
+
+# def fibo(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fibo(n-1) + fibo(n-2)
+
+
+# def fibonacci(n):
+#     for i in n:
+#         return i = n + n'
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+for i in range(100):
+    i = i + 1
+
+print(fibonacci(i))
+
+
+
+
