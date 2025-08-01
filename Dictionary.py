8fe0247e4aa1531e128b8913db6ac729cef8b324
kothambia <kothambiaaryan@gmail.com>
2025-08-01 23:38:56 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,21 @@
+#Dictionary 
+
+dic = {"EID01": "Gay",
+       "EID02": "Married",
+       "EID03": "Lost in Nether",
+       "EID04": "Single but ready to mingle",
+       "EID05": "Tushar the batman",
+       "EID06": "Not available",
+       "EID07": "Chudi jo khankiii hathoo meeein"
+}
+# for i in dic.items():
+#     print(i)
+
+# print(dic["EID03"])
+# print(dic.get("EID08"))
+
+# for i in dic.values():
+#     print(i)
+
+for i in dic.keys():
+     print(i)
