c796b980873d99455b3a99ca0547991a64971cbd
kothambia <kothambiaaryan@gmail.com>
2025-08-01 20:27:52 +0530

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,10 @@
+a = {1,2,3,4,5,5,6,2,2}
+print(a)
+
+print(type(a))
+
+for i in a:
+    print(i)
+
+aryan = set()
+print(type(aryan))
---
+++ setmethods.py
@@ -0,0 +1,39 @@
+#SET METHODS
+
+color1 = {"pink","orange","kalakhatta"}
+color2 = {"orange","kalakhatta","pink","black","pale","brown","rainbow","red","blue"}
+
+# print(color1.union(color2)) # both set fully added to new set
+# color1.update(color2)
+# print(color1) 
+# print(color1.intersection(color2)) #common in set
+# color2.intersection_update(color1)
+# print(color2)
+# print(color1.symmetric_difference(color2))
+# color1.symmetric_difference_update(color2)
+# print(color1)
+# print(color1.difference(color2))
+# color1.difference_update(color2)
+# print(color1)
+# print(color1.isdisjoint(color2))
+# print(color2.issuperset(color1))
+# print(color1.issubset(color2))
+# color1.add("white")
+# print(color1)
+# color1.remove("black")
+# print(color1)
+# color1.discard("black")
+# print(color1)
+# color1.pop()
+# print(color1)
+# del color1
+# print(color1)
+# color1.clear()
+# print(color1)
+if "grey" in color2:
+    print("yes")
+else:
+    print("no")
+
+
+
