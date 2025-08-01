65eeab8719bdc2416d0b6e07cd6e476309783608
kothambia <kothambiaaryan@gmail.com>
2025-08-01 21:14:21 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,40 @@
+def gmean (x,y):
+    mean=(x*y)/(x+y)
+    print(mean)
+
+def add(a,b):
+    sum=a+b
+    print(sum)
+
+def big(a,b):
+    if(a>b):
+        print("first value moti se")
+    else:
+        print("second value moti se")
+    
+def gragedefine(a):
+    pass
+
+
+gmean(26,55)
+
+a = 15
+b = 10
+gmean(a,b)
+add(a,b)
+big(a,b)
+
+
+x = 19 
+y = 30
+gmean(x,y)
+add(x,y)
+big(x,y)
+
+c = 212
+d = 28
+gmean(c,d)
+
+f = 27
+g = 73
+gmean(f,g)
---
+++ list methods.py
@@ -0,0 +1,30 @@
+
+g = ["Batman","Jocker","Harleyqueen","Robin","Catwomen"]
+g.append("Tushar")
+g.insert(3,"Aryan")
+b = ["ironman","hulk","thor"]
+g.extend(b)
+print(g)
+
+
+# d = [32,44,21,12,24,77,69,1,4,7,8,90]
+# d.sort()
+# print(d)
+# d.reverse()
+# print(d)
+
+# Attendence = ["mayank","tushar","sahil","dev","shruti","aditi","aryan","prayash"]
+
+# if "prayash" in Attendence:
+#         print("Present")
+# else:
+#         print("Farva gayo")
+# print(Attendence.index("prayash"))
+# list = [1,2,3,4,15,3,6,7]
+# print(list.count(3))
+# list2 = list.copy()
+# print(list2)
+
+color = ["blue","red","green","orange"]
+color2 = ["black","white","gold"]
+print(color+color2)
