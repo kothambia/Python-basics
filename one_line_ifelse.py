2c4a84236dc97e99a9bab59ddd51014c1753bcce
kothambia <kothambiaaryan@gmail.com>
2025-08-03 19:17:46 +0530

    Add files via upload

---
+++ one_line_ifelse.py
@@ -0,0 +1,8 @@
+
+Aryan = int(input(" Enter aura of Aryan:"))
+Tushar = int(input(" Enter aura of Tushar:"))
+
+print("Aryan has more aura") if Aryan>Tushar else print("both has Equal aura") if Aryan == Tushar else print("Tushar has more aura")
+
+A = "Superman" if Aryan<Tushar else print("Aryan is batman")
+print("Tushar is",A)
