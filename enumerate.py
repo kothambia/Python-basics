7a38580fa97ecb02e08b81012e80ae3f4d5feafb
kothambia <kothambiaaryan@gmail.com>
2025-08-04 20:09:08 +0530

    Add files via upload

---
+++ enumerate.py
@@ -0,0 +1,12 @@
+name = ["aryan","tushar","sizuka","hatori","miyoko","oggy","olivia"]
+
+# for i in range(len(name)):
+#     print(name[i])
+#     if (i==3):
+#         print("nami chuwan")
+#         i += i
+       
+for index,i in enumerate(name,start=2):
+    print(i)
+    if index==3:
+        print("nico robin")
