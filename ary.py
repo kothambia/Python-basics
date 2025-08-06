0d316eca6a31839d377b251da09d42ed0be93525
kothambia <kothambiaaryan@gmail.com>
2025-08-06 20:44:34 +0530

    Add files via upload

---
+++ ary.py
@@ -0,0 +1,10 @@
+def wel():
+    print("i am well thank you")
+
+aryan = "supreme and batman as well"
+print(aryan)
+
+if __name__ == "__main__":
+    wel()
+    
+    
---
+++ import.py
@@ -0,0 +1,12 @@
+import ary
+# import math
+
+ary.wel()
+print(ary.aryan)
+# # import time
+# a = int(input("enter a value:"))
+# # b = math.sqrt(a)
+# # print(b)
+# c = math.pow(a,3)
+# # print(dir(math))
+# print(c)
