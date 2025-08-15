fbd8bae26070bdbaeb5e223b2e467fd2c15ed5ad
kothambia <kothambiaaryan@gmail.com>
2025-08-15 13:45:34 +0530

    Add files via upload

--- constructor.py
+++ constructor.py
@@ -7,33 +7,26 @@ class car():
         self.tyre = tyre
         self.window = window
         self.company = company
+    def showinfo(self):
+        print(f"""                    customer Name   :       {self.name}
+                    customer id     :       {self.id}
+                    your car color  :       {self.color}
+                    your car body   :       {self.body}
+                    your car tyre   :       {self.tyre}
+                    your car window :       {self.window}
+                    your car company:       {self.company}
+""")
+
+class typeofcar(car):
+    def show(self):
+        print(f"{self.name} ni car a family car")
         
 d = car("1","Hasmukhbhai","red","metalic","ffs","opencustom","walkswagon")  
-print(f"""                    customer Name   :       {d.name}
-                    customer id     :       {d.id}
-                    your car color  :       {d.color}
-                    your car body   :       {d.body}
-                    your car tyre   :       {d.tyre}
-                    your car window :       {d.window}
-                    your car company:       {d.company}
-""")     
+d.showinfo()
 
-a = car("2","Tushar Rathva","green","fiber","mrf","matblack","bmw")
-print(f"""                    customer Name   :       {a.name}
-                    customer id     :       {a.id}
-                    your car color  :       {a.color}
-                    your car body   :       {a.body}
-                    your car tyre   :       {a.tyre}
-                    your car window :       {a.window}
-                    your car company:       {a.company}
-""")
+a = typeofcar("2","Tushar Rathva","green","fiber","mrf","matblack","bmw")
+a.showinfo()
+a.show()
+
 b = car("3","Aryan Kotham","black","platinum","opollo","default","gj23bombaridino")
-
-print(f"""                    customer Name   :       {b.name}
-                    customer id     :       {b.id}
-                    your car color  :       {b.color}
-                    your car body   :       {b.body}
-                    your car tyre   :       {b.tyre}
-                    your car window :       {b.window}
-                    your car company:       {b.company}
-""")
+b.showinfo()
